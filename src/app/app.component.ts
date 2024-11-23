5eb42877951f283e1a56050224cd572933571d30
Ashish Sharma <92317433+ashishaugust22@users.noreply.github.com>
2024-11-23 03:35:54 +0100

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,39 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {
+  Component,
+  WritableSignal,
+  Signal,
+  signal,
+  computed,
+  effect,
+} from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'ng-signals-practice';
+  computedCodeString: string = `doubleCount: Signal<number> = computed(() => this.count() * 2);`
+  effectCodeString: string = `    constructor() {
+        effect(() => {
+          this.simpleArray.push(
+            'Effect is triggered because count changed to' +  '$ {this.count()}'
+        );
+      });
+    }`
+  simpleArray: string[] = [];
+  count: WritableSignal<number> = signal(0);
+  doubleCount: Signal<number> = computed(() => this.count() * 2);
+
+  constructor() {
+    effect(() => {
+      this.simpleArray.push(
+        `Effect is triggered because count changed to ${this.count()}`
+      );
+    });
+  }
+  onIncreaseCount() {
+    this.count.update((currentValue) => currentValue + 1);
+  }
 }
